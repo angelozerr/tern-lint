265369d7023284e2c6c73865445d6cf3e348190d
angelozerr <angelo.zerr@gmail.com>
2014-10-27 12:55:19 +0100

    Fix https://github.com/angelozerr/tern-lint/issues/28

--- test/run.js
+++ test/run.js
@@ -1,13 +1,16 @@
 var util = require("./util");
 
+var IGNORE_UNUSED_VAR = {"rules" : {"UnusedVariable" : {"severity" : "none"}}};
+
 exports['test Known property'] = function() {
 	// Known property document.getElementById
 	util.assertLint("var elt = document.getElementById('myId');", {
 		messages : []
-	}, [ "browser" ]);
+	}, [ "browser" ], IGNORE_UNUSED_VAR);
 }
 
 exports['test Unknown property'] = function() {
+        
 	// Unknown property
 	util.assertLint("var elt = document.getElementByIdXXX('myId');", {
 		messages : [ {
@@ -16,9 +19,10 @@ exports['test Unknown property'] = function() {
 			"to" : 36,
 			"severity" : "warning"
 		} ]
-	}, [ "browser" ]);
+	}, [ "browser" ], IGNORE_UNUSED_VAR);
 	// Unknown property as error
-	var options = {"rules" : {"UnknownProperty" : {"severity" : "error"}}};
+	var options = {"rules" : {"UnknownProperty" : {"severity" : "error"}, 
+	                          "UnusedVariable" :  {"severity" : "none"}}};
 	util.assertLint("var elt = document.getElementByIdXXX('myId');", {
 		messages : [ {
 			"message" : "Unknown property 'getElementByIdXXX'",
@@ -40,9 +44,10 @@ exports['test Unknown identifier'] = function() {
 			"to" : 18,
 			"severity" : "warning"
 		} ]
-	});
+	}, null, IGNORE_UNUSED_VAR);
 	// Unknown identifier as error
-	var options = {"rules" : {"UnknownIdentifier" : {"severity" : "error"}}};
+	var options = {"rules" : {"UnknownIdentifier" : {"severity" : "error"},
+	                          "UnusedVariable" :  {"severity" : "none"}}};
 	util.assertLint("var elt = document.getElementById('myId');", {
 		messages : [ {
 			"message" : "Unknown identifier 'document'",
@@ -63,9 +68,10 @@ exports['test issue1'] = function() {
 			"to" : 30,
 			"severity" : "error"
 		} ]
-	}, [ "ecma5" ]);
+	}, [ "ecma5" ], IGNORE_UNUSED_VAR);
 	// Not a function as warning
-	var options = {"rules" : {"NotAFunction" : {"severity" : "warning"}}};
+	var options = {"rules" : {"NotAFunction" : {"severity" : "warning"},
+	                          "UnusedVariable" :  {"severity" : "none"}}};	
 	util.assertLint("var a = [];\nvar len = a.length();", {
 		messages : [ {
 			"message" : "'length' is not a function",
@@ -82,7 +88,7 @@ exports['test issue1'] = function() {
 			"to" : 30,
 			"severity" : "warning"
 		} ]
-	});
+	}, null, IGNORE_UNUSED_VAR);
 }
 
 exports['test issue2'] = function() {
@@ -99,11 +105,11 @@ exports['test issue2'] = function() {
 exports['test variables inside functions'] = function() {
 	util.assertLint("function test() { var a = {len: 5}; var len = a.len; }\nfunction b() { }", {
 		messages : [ ]
-	});
+	}, null, IGNORE_UNUSED_VAR);
 
 	util.assertLint("function b() { }\nfunction test() { var d = 5; var a = {len: 5}; var len = a.len; }", {
 		messages : [ ]
-	});
+	}, null, IGNORE_UNUSED_VAR);
 }
 
 
@@ -111,13 +117,13 @@ exports['test functions parameters'] = function() {
 	// In this case the type of `a` is inferred as a string
 	util.assertLint("function test(a) { var t = a; }; test('something');", {
 		messages : [ ]
-	});
+	}, null, IGNORE_UNUSED_VAR);
 
 	// In this case the type is unknown, but the variable is defined
 	// (should not produce a warning)
 	util.assertLint("function test(a) { var t = a; };", {
 		messages : [ ]
-	});
+	}, null, IGNORE_UNUSED_VAR);
 }
 
 
@@ -125,18 +131,18 @@ exports['test properties on functions parameters'] = function() {
 	// In this case the type of `a` is inferred as an object with a property `len`.
 	util.assertLint("function test(a) { var len = a.len; }; test({len: 5});", {
 		messages : [ ]
-	});
+	}, null, IGNORE_UNUSED_VAR);
 
 	// In this case the type of `a` is unknown, and should not produce warnings
 	// on any of its properties.
 	util.assertLint("function test(a) { var len = a.len; };", {
 		messages : [ ]
-	});
+	}, null, IGNORE_UNUSED_VAR);
 
 	// The same goes for function calls on an unknown type
 	util.assertLint("function test(a) { var len = a.myLength(); };", {
 		messages : [ ]
-	});
+	}, null, IGNORE_UNUSED_VAR);
 }
 
 exports['test assignment of unknown value'] = function() {
@@ -147,16 +153,16 @@ exports['test assignment of unknown value'] = function() {
 			"from": 41,
 			"to": 42,
 			"severity": "warning"} ]
-	});
+	}, null, IGNORE_UNUSED_VAR);
 
 	// The type of a.t is unknown, but it is still a valid property.
 	util.assertLint("var a = {}; function test(p) { a.t = p; var b = a.t; }", {
 		messages : [ ]
-	});
+	}, null, IGNORE_UNUSED_VAR);
 
 	util.assertLint("var a = {}; function test(p) { a.t = p; }", {
 		messages : [ ]
-	});
+	}, null, IGNORE_UNUSED_VAR);
 
 	// This should only contain a warning for `notdefined`, not for b = a.val.
 	util.assertLint("function A() {}; A.prototype.val = notdefined; var a = new A(); var b = a.val;", {
@@ -165,21 +171,21 @@ exports['test assignment of unknown value'] = function() {
 			"from": 35,
 			"to": 45,
 			"severity": "warning"} ]
-	});
+	}, null, IGNORE_UNUSED_VAR);
 
 	util.assertLint("var a = {t: 5}; function test(p) { a.t = p; }", {
 		messages : [ ]
-	});
+	}, null, IGNORE_UNUSED_VAR);
 }
 
 exports['test dynamic properties (bracket notation)'] = function() {
 	util.assertLint("var obj = { test: 1 }; var key = 'test'; var val1 = obj[key]; var val2 = obj['test'];", {
 		messages : [ ]
-	});
+	}, null, IGNORE_UNUSED_VAR);
 
 	util.assertLint("var obj = { test: function() {} }; var key = 'test'; obj[key](); obj['test']();", {
 		messages : [ ]
-	});
+	}, null, IGNORE_UNUSED_VAR);
 }
 
 exports['test type-checking of object literals declared before a function with a known type'] = function() {
@@ -187,39 +193,39 @@ exports['test type-checking of object literals declared before a function with a
   // declaration before the argument
   util.assertLint("var x = {id: 'fooID',hidden: true}; chrome.app.window.create('index.html', x);", {
           messages : []
-  }, [ "chrome_apps" ]);
+  }, [ "chrome_apps" ], IGNORE_UNUSED_VAR);
   
   // A Property that is unknown: anInvalidOption
   util.assertLint("var obj1 = {anInvalidOption: 'foo'}; chrome.app.window.create('index.html', obj1);", {
     "messages":[{"message":"Invalid argument at 2: anInvalidOption is not a property in CreateWindowOptions","from":12,"to":27,"severity":"error"}]
-  }, [ "chrome_apps" ]);
+  }, [ "chrome_apps" ], IGNORE_UNUSED_VAR);
   
   // A Property that does not type check, (hidden should be a bool)
   util.assertLint("var obj2 = {hidden: 'foo'}; chrome.app.window.create('index.html', obj2);", {
     "messages":[{"message":"Invalid argument at 2: cannot convert from String.prototype to Boolean.prototype","from":20,"to":25,"severity":"error"}]
-  }, [ "chrome_apps" ]);
+  }, [ "chrome_apps" ], IGNORE_UNUSED_VAR);
 
   // A Property that is of the correct type => OK
   util.assertLint("var obj3 = {id: 'fooID'}; chrome.app.window.create('index.html', obj3);", {
     messages : []
-  }, [ "chrome_apps" ]);
+  }, [ "chrome_apps" ], IGNORE_UNUSED_VAR);
   
   // multiple Properties of the correct type => OK
   util.assertLint("var obj4 = {id: 'fooID',hidden: true}; chrome.app.window.create('index.html', obj4);", {
     messages : []
-  }, [ "chrome_apps" ]);
+  }, [ "chrome_apps" ], IGNORE_UNUSED_VAR);
   
   // one bad one mixed in
   util.assertLint("var obj5 = {id: 'fooID', oho: 34, hidden: true}; chrome.app.window.create('index.html', obj5);", {
     "messages":[{"message":"Invalid argument at 2: oho is not a property in CreateWindowOptions","from":25,"to":28,"severity":"error"}]
-  }, [ "chrome_apps" ]);
+  }, [ "chrome_apps" ], IGNORE_UNUSED_VAR);
 
   // id is forward declared object literal but it should be string
   util.assertLint("var lit = {foo: 400}; document.getElementById(lit);", {
           messages : [ {
             "message":"Invalid argument at 1: cannot convert from Object.prototype to String.prototype","from":46,"to":49,"severity":"error"
           }]
-  }, [ "browser" ]);
+  }, [ "browser" ], IGNORE_UNUSED_VAR);
 }
 
 exports['test Invalid Argument'] = function() {
@@ -229,7 +235,7 @@ exports['test Invalid Argument'] = function() {
           messages : [ {
             "message":"Invalid argument at 1: cannot convert from Object.prototype to String.prototype","from":34,"to":44,"severity":"error"
           }]
-  }, [ "browser" ]);  
+  }, [ "browser" ], IGNORE_UNUSED_VAR);  
   
   // #JSObjectLiteralInParameter
   // a Property that is unknown: anInvalidOption
@@ -240,7 +246,7 @@ exports['test Invalid Argument'] = function() {
           "to":55,
           "severity":"error"
           }]
-  }, [ "chrome_apps" ]);
+  }, [ "chrome_apps" ], IGNORE_UNUSED_VAR);
   // a Property that does not type check, (hidden should be a bool)
   util.assertLint("chrome.app.window.create('index.html', {hidden: 'foo'});", {
           messages : [{
@@ -249,15 +255,15 @@ exports['test Invalid Argument'] = function() {
             "to":53,
             "severity":"error"
           }]
-  }, [ "chrome_apps" ]);
+  }, [ "chrome_apps" ], IGNORE_UNUSED_VAR);
   // a Property that is of the correct type => OK
   util.assertLint("chrome.app.window.create('index.html', {id: 'fooID'});", {
           messages : []
-  }, [ "chrome_apps" ]);
+  }, [ "chrome_apps" ], IGNORE_UNUSED_VAR);
   // multiple Properties of the correct type => OK
   util.assertLint("chrome.app.window.create('index.html', {id: 'fooID',hidden: true});", {
           messages : []
-  }, [ "chrome_apps" ]);
+  }, [ "chrome_apps" ], IGNORE_UNUSED_VAR);
   // one bad one mixed in
   util.assertLint("chrome.app.window.create('index.html', {id: 'fooID', oho: 34, hidden: true});", {
           messages : [{
@@ -266,18 +272,16 @@ exports['test Invalid Argument'] = function() {
           "to":56,
           "severity":"error"
           }]
-  }, [ "chrome_apps" ]);
-
-  
+  }, [ "chrome_apps" ], IGNORE_UNUSED_VAR);
 
   // id is string => OK 
   util.assertLint("var elt = document.getElementById('100');", {
           messages : []  
-  }, [ "browser" ]);  
+  }, [ "browser" ], IGNORE_UNUSED_VAR);  
   // id is string => OK 
   util.assertLint("var id = '100'; var elt = document.getElementById(id);", {
           messages : []  
-  }, [ "browser" ]);
+  }, [ "browser" ], IGNORE_UNUSED_VAR);
   // id is number although it should be string 
   util.assertLint("var elt = document.getElementById(100);", {
           messages : [ {
@@ -285,7 +289,7 @@ exports['test Invalid Argument'] = function() {
             "from": 34,
             "to": 37,
             "severity": "error"}]
-  }, [ "browser" ]);
+  }, [ "browser" ], IGNORE_UNUSED_VAR);
   // id is number although it should be string 
   util.assertLint("var id = 100; var elt = document.getElementById(id);", {
           messages : [ {
@@ -293,11 +297,11 @@ exports['test Invalid Argument'] = function() {
             "from": 48,
             "to": 50,
             "severity": "error"}]
-  }, [ "browser" ]);
+  }, [ "browser" ], IGNORE_UNUSED_VAR);
   // listener must be a function => OK 
   util.assertLint("var f = function() {}; document.addEventListener('click', f, true)", {
           messages : []
-  }, [ "browser" ]);  
+  }, [ "browser" ], IGNORE_UNUSED_VAR);  
   // listener must be a function but argument is a boolean 
   util.assertLint("var b = true; document.addEventListener('click', b, true)", {
           messages : [{
@@ -305,11 +309,11 @@ exports['test Invalid Argument'] = function() {
             "from": 49,
             "to": 50,
             "severity": "error"}]
-  }, [ "browser" ]);
+  }, [ "browser" ], IGNORE_UNUSED_VAR);
   // null argument => OK 
   util.assertLint("var elt = document.getElementById(null);", {
           messages : []
-  }, [ "browser" ]);
+  }, [ "browser" ], IGNORE_UNUSED_VAR);
   // Unknown argument => throw error widh Unknown identifier
   util.assertLint("var elt = document.getElementById(xxx);", {
           messages : [{
@@ -318,7 +322,7 @@ exports['test Invalid Argument'] = function() {
             "to" : 37,
             "severity" : "warning"
     }]
-  }, [ "browser" ]); 
+  }, [ "browser" ], IGNORE_UNUSED_VAR); 
   
   // isEqualNode waits Node.prototype and document.getElementById returns Element.prototype which extends Node.prototype => OK
   util.assertLint("document.getElementById('id1').isEqualNode(document.getElementById('id2'))", {
@@ -335,6 +339,46 @@ exports['test Invalid Argument'] = function() {
     }]
   }, [ "ecma5" ]); 
   
+}
+
+exports['test Unused variable'] = function() {
+  // Unknown property
+  util.assertLint("var id1='100';\n" +
+                  "var id2='100'; console.log(id2);\n" +
+                  "var id3='100', id4 = id3;", {
+          messages : [ {
+                  "message" : "Unused variable 'id1'",
+                  "from" : 4,
+                  "to" : 7,
+                  "severity" : "warning"
+                  },{
+                  "message" : "Unused variable 'id4'",
+                  "from" : 63,
+                  "to" : 66,
+                  "severity" : "warning"
+          } ]
+  }, [ "browser" ]);    
+}
+
+exports['test Unused function'] = function() {
+  // Unknown property
+  util.assertLint("var f=function(){};\n" +
+                  "var g=function(){};\ng();\n" + 
+                  "function f1(){};\n" + 
+                  "function g1(){};\ng1();\n", {
+          messages : [ {
+                  "message" : "Unused variable 'f'",
+                  "from" : 4,
+                  "to" : 5,
+                  "severity" : "warning"
+                  },
+                  {
+                    "message" : "Unused function 'f1'",
+                    "from" : 54,
+                    "to" : 56,
+                    "severity" : "warning"
+            }]
+  }, [ "browser" ]);    
 }
 
 if (module == require.main)
