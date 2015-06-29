ee5bc3be955188781f60ac4fd4a15bc5b757e7d1
angelozerr <angelo.zerr@gmail.com>
2015-06-29 05:33:00 +0200

    Add RequireJS test.

---
+++ test/requirejs/validate_config.js
@@ -0,0 +1,17 @@
+var util = require("../util");
+require("tern/plugin/requirejs");
+
+var IGNORE_UNUSED_VAR = {"rules" : {"UnusedVariable" : {"severity" : "none"}}};
+
+exports['test RequireJS config'] = function() {
+   
+    // Boolean
+    util.assertLint("requirejs.config({baseUrl:'', XXXX:true})", {
+        messages : [{"message":"Invalid property at 2: XXXX is not a property in Object.prototype",
+                     "from":30,
+                     "to":34,
+                     "severity":"error",
+                     "file":"test1.js"}]
+    }, [ "ecma5", "browser" ], { "requirejs" : {}}, IGNORE_UNUSED_VAR, true);
+    
+}
