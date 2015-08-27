3263d1139d5c39cff2420829c094f1e2a5c60dc3
Dmitrii Kovchin <kovchin@google.com>
2015-08-27 12:47:59 -0700

    https://github.com/angelozerr/tern-lint/issues/50

--- test/validate_multitypes.js
+++ test/validate_multitypes.js
@@ -12,3 +12,46 @@ exports['test issue36'] = function() {
           "file":"test1.js"}]
     }, [ "ecma5" ], null, IGNORE_UNUSED_VAR);
 }
+
+
+exports['test Inheritance Types'] = function() {
+  // https://github.com/angelozerr/tern-lint/issues/50
+  util.assertLint(
+      "function Person(name) {\n"
+      +"  this.name = name;\n"
+      +"}\n"
+      +"\n"
+      +"function Employee(name, title) {\n"
+      +"  this.constructor(name);\n"
+      +"  this.title = title;\n"
+      +"}\n"
+      +"\n"
+      +"Employee.prototype = new Person();\n"
+      +"Employee.prototype.constructor = Person;\n"
+      +"\n"
+      +"function Customer(name) {\n"
+      +"  this.constructor(name);\n"
+      +"}\n"
+      +"\n"
+      +"Customer.prototype = new Person();\n"
+      +"Customer.prototype.constructor = Person;\n"
+      +"\n"
+      +"\n"
+      +"function createSomebody(numb) {\n"
+      +"  if (numb % 2 === 1) {\n"
+      +"    return new Employee('me', 'worker');\n"
+      +"  } else {\n"
+      +"    return new Customer('notMe');\n"
+      +"  }\n"
+      +"}\n"
+      +"\n"
+      +"alert(createSomebody(2)); ", {
+        messages : [{
+          "message" : "Invalid argument at 1: cannot convert from Employee|Customer to string",
+          "from" : 496,
+          "to" : 510,
+          "severity" : "error",
+          "file": "test1.js"
+        }]
+      }, [ "browser" ]);
+}
