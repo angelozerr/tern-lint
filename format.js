bfcdc2f0e5fd080875726a11327ab29578b61480
David O'Trakoun <dotrakoun@elitedaily.com>
2016-09-30 14:18:15 -0400

    colon only

--- format.js
+++ format.js
@@ -34,8 +34,7 @@ exports.vim = function (data) {
 
   return data.messages.map(function (m) {
     return [
-      m.file,
-      (m.from.line - 1) + ':' + (m.from.ch),
+      m.file + ':' + (m.from.line - 1) + ':' + (m.from.ch),
       m.severity,
       m.message,
     ].join(': ');
